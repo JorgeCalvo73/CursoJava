8e263dafa0a2a7204a698197d86c66da3accf355
Jorge Calvo <calvoblancojorge@gmail.com>
2021-05-31 15:53:24 +0200

    javaspring segundo commit

--- javaspring/src/main/java/com/curso/javaspring/conf/ConfiguracionMVC.java
+++ javaspring/src/main/java/com/curso/javaspring/conf/ConfiguracionMVC.java
@@ -1,5 +1,26 @@
 package com.curso.javaspring.conf;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan
+@Configuration
 public class ConfiguracionMVC {
+
+	public ConfiguracionMVC() {
+		super();
+		System.out.println("Creando una instancia de ConfiguracionMVC");
+	}
+	
+	public ViewResolver viewResolver() {
+		
+		System.out.println("Instanciando el ViewResolver");
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/paginas/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
 
 }
--- javaspring/src/main/java/com/curso/javaspring/controller/FormularioClientesController.java
+++ javaspring/src/main/java/com/curso/javaspring/controller/FormularioClientesController.java
@@ -1,8 +1,10 @@
 package com.curso.javaspring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class FormularioClientesController {
 
 	@GetMapping("/formularioClientes")
